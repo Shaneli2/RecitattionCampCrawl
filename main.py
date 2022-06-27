631e9640040795e7f328e3e11dc21f0394304fc0
Shaneli2 <276433574@qq.com>
2022-06-27 18:32:52 +0800

    complete 方法与实战

--- main.py
+++ main.py
@@ -277,6 +277,71 @@ def getwords(url):
                         print(f'url-article is {url_article}')
                         # getSubSeries(url_article)
 
+def getwords2(url):
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37",
+        "cookie": "___rl__test__cookies=1656309923152; _ntes_nnid=d210a1ed73ac8d49791dd9e007b8a7e7,1634192367969; _ntes_nuid=d210a1ed73ac8d49791dd9e007b8a7e7; UM_distinctid=17fbfa77430187-0d1d26d2b671fc-5617185b-100200-17fbfa77431f41; vinfo_n_f_l_n3=3cc07e6b80b1c971.1.0.1648187892685.0.1648187967384; EDUWEBDEVICE=398a0755db534b7f8b428017d18913a2; EDU-YKT-MODULE_GLOBAL_PRIVACY_DIALOG=true; sideBarPost=1970; xuetangvendor=ykt_store_xiaomi; keoutvendor=; ke_Pdt=ydkWeb; ke_inLoc=; OUTFOX_SEARCH_USER_ID_NCOO=1288925762.7441041; NTES_YD_PASSPORT=1MO6bF40jEdNWQmQ2SrNYG.BnXt1obLPnVlWa0Jp.NnlceKDcQzbiWZz7LqFIzWscvbNIuHnb9RDQK9sLCGpRVd.7o51FRZ7XFbDBOFGCrKDL9LIabrmqyO.KoVrMukkvgldIL97L8WolUSEVMb91EuNp6w9vqfnN_MyUUt.vzDy2xwScGHR18UfD5xbPtuR8W0r2kYL9xiATUg8xmzT9A5Hx; P_INFO=13828460875|1656223239|1|study|00&99|null&null&null#gud&440100#10#0#0|&0|null|13828460875; NTESSTUDYSI=ab92a0adcd0e4c08b003af4a038a0db1; STUDY_INFO=\"yd.a3d7bb5d90db452b8@163.com|8|1453061710|1656309682137\"; STUDY_SESS=\"ewwZXGPJCgIdhQZyKlAivbZNqDfdcnEYZC/3PYNvIPK7ARHbjp0KbtBiBQalOelEd576j8fTOQCEVtBIlPoDXqF5GlQExgPUf7V7um5mOPDnQnlS2AxfdgHgZzoYiNLgxX/PMLObuQ42wEi+5bapRia6P6tFClHMzzmWa6WFFTULhur2Nm2wEb9HcEikV+3FTI8+lZKyHhiycNQo+g+/oA==\"; STUDY_PERSIST=\"kR1jDZcxnohSw5v8UKhRctaBEHFrHRM0wpAW6CZRNp3Us6MLk+u/5dDs7I7+wuwLRYyCfO7dmeYkYH9evgZ+TVGaQ6FMiwp7siATddSBWseyDNVsUJeKH4fkKfxxHQZ/ehXrCCy57DlSvlHpuW1E19nHRcWf3u0TGyFfthj3Qv4fEek0ZhdcJxTG1W7arGlZKHMP4hWNtrKY5k0E7e39qmJy8j26A3t0u09U6yAvTDfZgpjCC7Iso4RP9U87vJE8LtaQzUT1ovP2MqtW5+L3Hw+PvH8+tZRDonbf7gEH7JU=\"; DICT_SESS=v2|C72VrrzF2RkGn4YEhHPLRJBk4UMkfQS0ez0HJyh4pBR6BO4YGRHTu06uOMqLRHpK0zMkfUMkMYm0gK0HUfRMTF0zYO4TS0fYE0; DICT_LOGIN=1||1656309682196; NETEASE_WDA_UID=1453061710#|#1611456020195; NTES_STUDY_YUNXIN_ACCID=s-1453061710; NTES_STUDY_YUNXIN_TOKEN=09261a82a7d73dbe17ce8b7aa6377359; utm=eyJjIjoiIiwiY3QiOiIiLCJpIjoiIiwibSI6IiIsInMiOiIiLCJ0IjoiIn0=|aHR0cHM6Ly9zdHVkeS4xNjMuY29tLw==; Hm_lvt_e46790f6d676f71878ebef1153dda3e2=1655953652,1656002346,1656210125,1656309709; hb_MA-BFF5-63705950A31C_source=ke.study.163.com; Hm_lpvt_e46790f6d676f71878ebef1153dda3e2=1656309923; JSESSIONID=6FA4FFBA18B4642BE39D17AA60CBBDA7"
+    }
+
+    resp = requests.get(url, headers=headers)
+    # print(resp.text)
+
+    f = open(f"downloadlist2.txt", mode="w")
+    #https://live.study.163.com/live/index.html?courseId=59083&lesson=3045085&type=1
+    obj = re.compile(r'window.lesson =(?P<lst>.*}])', re.S)
+    tmp = obj.finditer(resp.text)
+    for it in tmp:
+        lst = it.group("lst")
+    res = json.loads(lst)
+    # res is list which only has one element i.e dict   方法与实战
+    print(res)
+    title0 = res[0]["title"] # 方法与实战
+    for it in res[0]["list"]:  # ie. 方法篇,  实战篇
+
+        # print(type(it))
+        title1 = it.get('title')  # 方法篇,  实战篇
+        print(f"{title0}_{title1}*********************************************************************")
+        print(it)
+
+        for i in it['list']: # i is each section e.g '了解这条基本原则，发现背词新大陆' under '方法篇'
+            title1_1 = i.get('title').replace('|','') # '了解这条基本原则，发现背词新大陆'
+            print(f"{title0}_{title1}_{title1_1}*************************************************")
+            if i.get('video'):
+                id1_1_1 = i.get('id')
+                # https://ke.study.163.com/course/video.json?lessonId=3045085&r=1656251042305
+                url_video = f'https://ke.study.163.com/course/video.json?lessonId={id1_1_1}'
+                print(url_video)
+                resp_video = requests.get(url_video, headers=headers)
+                # resp_video.encoding = resp_video.apparent_encoding
+                # print(resp_video.text)
+
+                if not resp_video.json() or resp_video.json().get('success') is False:
+                    print(f'Not data returned from url {url_video}')
+                    continue
+
+                url_mp4 = resp_video.json().get('result').get('videoUrl')
+                print(f"url_mp4 is {url_mp4}")
+                title1_1_1_ = resp_video.json().get('result').get('lessonTitle').replace('|', '')
+                lessonTitle = f'{title1}_{title1_1_1_}'
+                print(f'get {lessonTitle} video data starting.......')
+                resp_video.close()
+
+                # download
+                resp_mp4 = requests.get(url_mp4, headers=headers)
+                # with open(f"{lessonTitle}.mp4", mode="wb") as f:
+                #     f.write(resp_mp4.content)
+                # print(f'{lessonTitle}.mp4 saved')
+                resp_mp4.close()
+
+                # further handle mp4 as hit 403 error
+
+                f.write(f'{url_mp4}                                                             {lessonTitle}\n')
+                print(f'{url_mp4}        {lessonTitle} writen')
+
+
+
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     url = r'https://ke.study.163.com/course/detail/59009'
@@ -286,7 +351,10 @@ if __name__ == '__main__':
     # 每周精讲
     # getRecitationCamp2(url)
     # 英语学习必备5500词（背诵营18季）
-    url_w =r'https://ke.study.163.com/course/detail/59079'
-    getwords(url_w)
+    # url_w =r'https://ke.study.163.com/course/detail/59079'
+    # getwords(url_w)
+    # 杨亮讲单词·方法与实战
+    url_w2 = r'https://ke.study.163.com/course/detail/59083'
+    getwords2(url_w2)
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
